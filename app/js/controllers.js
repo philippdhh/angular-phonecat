fe8657a50d04c6c6525c8d41fdc6d0703cd941b2
Philipp Dudda <mail@philippd.de>
2015-04-02 10:01:18 +0200

    ongoing tutorial

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,25 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = new angular.module('phonecatApp',[]);
+
+phonecatApp.controller('PhoneListCtrl', function($scope){
+    $scope.phones = [
+    {
+        'name': 'iPhone',
+        'snippet': 'Gutes Telefongerät!'
+    },
+    {
+        'name': 'Samsung Quatsch',
+        'snippet':  'Für die Tonne!'
+    }
+        ];
+    $scope.persons = [
+        {
+            'firstName': 'Philipp',
+            'lastName': 'Dudda'
+        }
+    ];
+    $scope.currentPerson = "Hallo";
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -9,3 +9,16 @@ describe('controllers', function() {
   });
 
 });
+
+describe('PhoneListCtrl', function() {
+  beforeEach(module('phonecatApp'));
+
+  it('should create phone model with 2 phones', inject(function($controller) {
+    var scope = {};
+    ctrl = $controller('PhoneListCtrl', {$scope: scope});
+
+    9(scope.phones.length).toBe(2);
+
+  }));
+
+});
